bc99abeb7656fbdcd9c909f658bc22ed92039a6b
Ben Abbott <jaminscript@gmail.com>
2015-08-18 14:20:49 +1200

    Add SQL file with some users

--- assets/ajax_live_validation.sql
+++ assets/ajax_live_validation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 18, 2015 at 02:44 am
+-- Generation Time: Aug 18, 2015 at 04:20 am
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -30,7 +30,15 @@ CREATE TABLE IF NOT EXISTS `users` (
 `ID` int(10) unsigned NOT NULL,
   `username` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `username`, `email`) VALUES
+(3, 'admin', 'admin@admin.com'),
+(4, 'iambatman', 'bat@man.com');
 
 --
 -- Indexes for dumped tables
@@ -50,7 +58,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
