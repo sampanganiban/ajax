49cfa7ce98be67336999ff1d6e29dbbf55269735
Ben Abbott <jaminscript@gmail.com>
2015-08-18 13:26:19 +1200

    Listen to username input field and do basic validation

--- js/live-validation.js
+++ js/live-validation.js
@@ -1,5 +1,21 @@
 $('document').ready(function(){
 
-	
+	// Listen to the username input field
+	$('#username').blur( checkUsername );
 
 });
+
+function checkUsername() {
+
+	// Obtain the username
+	var username = $(this).val();
+
+	// Leave if the username is blank
+	if( username.length < 5 ) {
+		$('#username-message').html('Needs to be at least 5 characters');
+		return;
+	} else {
+		$('#username-message').html('');
+	}
+
+}
