261c692b0e16873ecd8a7baadaa08a83119264bf
Ben Abbott <jaminscript@gmail.com>
2015-08-19 14:39:22 +1200

    Add PHP that queries the Database for a user based on ID and return JSON

--- customer-info/app/get-customer-info.php
+++ customer-info/app/get-customer-info.php
@@ -1,3 +1,43 @@
 <?php
 
-echo 'Hello from PHP '.$_GET['customerID'];
+// Connect to the database
+$dbc = new mysqli('localhost', 'root', '', 'ajax_customers');
+
+// Filter the ID
+$customerID = $dbc->real_escape_string( $_GET['customerID'] );
+
+// Prepare the SQL
+$sql = "SELECT phone, email
+		FROM customers
+		WHERE id = $customerID";
+
+// Run the query
+$resultDB = $dbc->query($sql);
+
+// If there was a result
+if( $resultDB->num_rows == 1 ) {
+
+	// Convert the result into an associative array
+	$resultASSOC = $resultDB->fetch_assoc();
+
+	// Convert the result into JSON
+	$resultJSON = json_encode($resultASSOC);
+
+	// Set up the header so JavaScript knows that we are sending JSON
+	header('Content-Type: application/json');
+
+	// Send the data back to JavaScript
+	echo $resultJSON;
+
+} else {
+
+
+
+}
+
+
+
+
+
+
+
